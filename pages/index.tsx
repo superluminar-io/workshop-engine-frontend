0a4ef065e9144bab3b976331b9a11e385d5f92e1
Henrik Fricke <henrikfricke@web.de>
2022-04-11 21:52:30 +0200

    refactor(Layout): add option to have an element on the right side

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import { useClerk } from "@clerk/nextjs";
 import { Layout } from "../src/components/Layout/Layout";
 import { WorkshopListContainer } from "../src/containers/WorkshopListContainer/WorkshopListContainer";
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 const Home = () => {
   const { signOut } = useClerk();
 
   return (
-    <Layout title="Welcome 👋" signOut={() => signOut()}>
+    <Layout
+      title="Welcome 👋"
+      titleRightSide={
+        <Button
+          size="small"
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+        >
+          Create workshop
+        </Button>
+      }
+      signOut={() => signOut()}
+    >
       <WorkshopListContainer />
     </Layout>
   );
